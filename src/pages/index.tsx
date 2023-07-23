03f650837fc585e60be3017d4518eefb5c67fea8
0sunman <youyoungsun2000@gmail.com>
2023-07-23 23:39:10 +0900

    content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,6 +29,12 @@ export default function Home() {
             <h2>15%</h2>
             <span>SECHIHYEO</span>
           </div>}/>
+          <BannerComponent
+            imgUrl={"url(//img.cjthemarket.com/images/file/product/160/20210625174933839.png?SF=webp&RS=350x350)"}
+            Title={"황금색 감자튀김의 맛있는 유혹"}
+            Text={"바삭한 겉은 입안에서 살살 녹아내리고, 부드럽고 달콤한 감자 속은 어느새 두 번 다시 찾게 될 맛있는 유혹으로 여러분을 초대합니다."}
+            />
+
           <BannerComponent 
           Title={"완벽한 바캉스 보낼 준비"}
           Text={"뜨거운 날씨와 여름 비 소식으로 가늠할 수 없다면 주목하세요. 다양한 아이템을 최대 88% 할인율로 준비했습니다. 시원한 쇼핑을 위한 12% 쿠폰도 놓치지 마세요."}
@@ -37,6 +43,12 @@ export default function Home() {
             <h2>15%</h2>
             <p>SUMMER VACANCE</p>
           </div>}/>
+          <BannerComponent
+            imgUrl={"url(//img.cjthemarket.com/images/file/product/560/20221228152644244.jpg?SF=webp&RS=350x350)"}
+            Title={"입맛을 사로잡는 떡볶이"}
+            Text={"입안 가득 퍼지는 풍부한 양념의 균형과 쫄깃한 떡의 신선함이 만났을 때, 특별한 순간을 위한 특별한 맛, 지금 경험해보세요!"}
+            />
+            
 
 <div style={{marginBottom:"100px"}}></div>
         <BottomComponent/>
